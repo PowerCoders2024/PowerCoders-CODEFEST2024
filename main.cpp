939baca032ae8530e5cf1c615504994f2cf9c4f7
alejandroP03 <d.pulidob@uniandes.edu.co>
2024-07-27 00:57:53 -0500

    Medallo es loma lo mas parecido a un skatepark

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/aes.h>
 #include <wolfssl/wolfcrypt/ecc.h>
 #include <wolfssl/wolfcrypt/random.h>
 #include <wolfssl/wolfcrypt/settings.h>
@@ -7,12 +8,14 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 // Declaraciones de funciones
 void encrypt(const std::string& input_path, const std::string& output_path);
 void decrypt(const std::string& input_path, const std::string& output_path);
-void init_keys(byte secret[]);
+void init_keys(byte secret[AES_128_KEY_SIZE]);
+void encrypt_message(byte key[]);
 void get_public_key(ecc_key& pub);
 
 int main(int argc, char* argv[]) {
@@ -42,21 +45,23 @@ void encrypt(const std::string& input_path, const std::string& output_path) {
   std::cout << "input_path=" << input_path << std::endl;
   std::cout << "output_path=" << output_path << std::endl;
 
-  byte secret[1024];
+  byte secret[AES_128_KEY_SIZE];
   init_keys(secret);
+  encrypt_message(secret);
   std::cout << "Encrypted image" << std::endl;
 }
 
-void init_keys(byte secret[]) {
+void init_keys(byte secret[AES_128_KEY_SIZE]) {
   ecc_key priv, pub;
   WC_RNG rng;
 
-  word32 secretSz = sizeof(secret);
+  word32 secretSz = AES_128_KEY_SIZE;
   int ret;
 
-  wc_InitRng(&rng);                  // initialize rng
-  wc_ecc_init(&priv);                // initialize key
-  wc_ecc_make_key(&rng, 32, &priv);  // make public/private key pair
+  wc_InitRng(&rng);    // initialize rng
+  wc_ecc_init(&priv);  // initialize key
+  wc_ecc_make_key(&rng, AES_128_KEY_SIZE,
+                  &priv);  // make public/private key pair
   // receive public key, and initialise into pub
   get_public_key(pub);
   ret = wc_ecc_shared_secret(&priv, &pub, secret, &secretSz);
@@ -65,6 +70,33 @@ void init_keys(byte secret[]) {
   if (ret != 0) {
     // error generating shared secret key
   }
+}
+
+void encrypt_message(byte key[]) {
+  int res;
+  Aes aes;
+  wc_AesInit(&aes, NULL, 0);
+  res = wc_AesGcmSetKey(&aes, key, 16);
+
+  byte plain[AES_BLOCK_SIZE * 50];
+  std::stringstream ss("Westcol vendiendo empanadas");
+  ss.read((char*)plain, sizeof(plain));
+
+  byte cipher[sizeof(plain)];
+  byte decrypted[sizeof(plain)];
+  byte iv[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+               0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+  byte authTag[16];
+  byte authIn[16] = {0};
+  wc_AesGcmEncrypt(&aes, cipher, plain, sizeof(cipher), iv, sizeof(iv), authTag,
+                   sizeof(authTag), authIn, sizeof(authIn));
+
+  wc_AesGcmDecrypt(&aes, decrypted, cipher, sizeof(cipher), iv, sizeof(iv),
+                   authTag, sizeof(authTag), authIn, sizeof(authIn));
+
+  std::cout << plain << std::endl;
+  std::cout << cipher << std::endl;
+  std::cout << decrypted << std::endl;
 }
 
 void get_public_key(ecc_key& pub) {
