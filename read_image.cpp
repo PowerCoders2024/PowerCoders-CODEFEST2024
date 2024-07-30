88a236998b905ab6b7266fb2437c8f928c9a953e
alejandroP03 <d.pulidob@uniandes.edu.co>
2024-07-29 23:54:27 -0500

    Imagen recibida con threads

--- read_image.cpp
+++ read_image.cpp
@@ -1,47 +1,88 @@
-#include <barrier>
+
 #include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
+#include <vector>
+#include <condition_variable>
+
+
 #define THREAD_POOL_SIZE 10
 
-void encrypt(char buff[], char out[], unsigned long size, int thread_id,
-             auto &sync_barrier) {
-  std::cout << "Encrypting thread #" << thread_id << ":..." << std::endl;
-  for (int i = 0; i < size; i++) out[i] = buff[i];
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  std::cout << "Encrypted thread #" << thread_id << std::endl;
+void encrypt(int thread_id, uint64_t size_buff, char* buff,
+             std::fstream& out_file_stream, std::mutex& mtx,
+             int& mutex_counter, std::condition_variable& cv) {
+  for (int i = 0; i < size_buff; i++) buff[i] = buff[i];  // Encrypt func
+
+  while (mutex_counter != thread_id) {
+    mtx.unlock();
+
+  }
+
+  std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [thread_id, mutex_counter]{ return mutex_counter == thread_id; });
+    out_file_stream.write(buff, size_buff);
+
+    // Avanzar al siguiente hilo
+    mutex_counter++;
+    cv.notify_all();
+
+    delete[] buff;
+  
 }
 
-int main(int argc, char *argv[]) {
-  std::string filename = argv[1];
+uint64_t open_file(std::string& filename, std::fstream& file_stream) {
   std::filesystem::path p(filename);
   std::cout << filename << std::endl;
 
-  unsigned long file_size = std::filesystem::file_size(p);
+  uint64_t file_size;
+  if (std::filesystem::exists(p))
+    file_size = std::filesystem::file_size(p);
+  else
+    file_size = 0;
+
   std::cout << "File size: " << file_size << std::endl;
-  std::fstream file_stream(filename);
-  unsigned long buff_size = file_size / (THREAD_POOL_SIZE - 1);
+  file_stream.open(filename);
+  return file_size;
+}
+
+int main(int argc, char* argv[]) {
+  std::string in_filename = argv[1];
+  std::string out_filename = argv[2];
+  std::fstream in_file_stream;
+  std::fstream out_file_stream;
+
+  uint64_t file_size = open_file(in_filename, in_file_stream);
+  open_file(out_filename, out_file_stream);
+
+  uint64_t buff_size = file_size / (THREAD_POOL_SIZE - 1);
 
   int total = 0;
 
-  std::barrier sync_barrier(
-      THREAD_POOL_SIZE);  // También se puede declarar una función para cuando
-                          // se llegue a la barrera
+  std::thread threads[THREAD_POOL_SIZE];
+  
+  std::condition_variable cv;
+  std::mutex mtx;
+  int mutex_counter = 0;
   for (int i = 0; i < THREAD_POOL_SIZE; i++) {
-    char buffer[buff_size];  // Probablemente haya que borrarlo despues de cada
-                             // iteración
-    file_stream.read(buffer, buff_size);
-    std::cout << "Read " << file_stream.gcount() << " bytes" << std::endl;
-    std::thread t([&]() {
-      encrypt(buffer, buffer, buff_size, i, sync_barrier);
-    });  // El & de la función lambda indica que el scope de la función lambda
-         // es el mismo que el scope de la función main
-    t.join();
-    total += file_stream.gcount();
+    
+
+    char* buffer = new char[buff_size];
+
+    in_file_stream.read(buffer, buff_size);
+    uint64_t read = in_file_stream.gcount();
+
+    std::thread t(encrypt, i, read, buffer, std::ref(out_file_stream),
+                  std::ref(mtx), std::ref(mutex_counter), std::ref(cv));
+
+    threads[i] = std::move(t);
+    total += read;
   }
+
+  for (int i = 0; i < THREAD_POOL_SIZE; i++) threads[i].join();
 
   std::cout << "Total read: " << total << std::endl;
 
